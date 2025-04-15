58edea2834fbe5207ee8ece1376b34a3be41e2e8
jungin <jungin@users.noreply.github.com>
2025-04-15 16:34:52 -0500

    changed model structure

--- train.py
+++ train.py
@@ -5,6 +5,9 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from utility import load_data
 
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
 from variables import preprocessed_train_dir, preprocessed_val_dir,\
                       train_csv, val_csv, \
                       JESTER_CLASSES, \
@@ -23,25 +26,40 @@ val_label_file = val_csv                             # 라벨 CSV 경로
 X_train, y_train = load_data(train_label_file, train_dir)
 X_val, y_val = load_data(val_label_file, val_dir)
 
-import tensorflow as tf
-from tensorflow.keras import layers, models
 
-model = models.Sequential([
-    layers.Input(shape=(37, 63)),
-    layers.Conv1D(64, kernel_size=3, activation='relu', padding='same'),
-    layers.MaxPooling1D(pool_size=2),
-    layers.Conv1D(128, kernel_size=3, activation='relu', padding='same'),
-    layers.GlobalAveragePooling1D(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(JESTER_CLASSES, activation='softmax')
-])
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.summary()
+def build_tcn_model(input_shape, num_classes):
+    inputs = layers.Input(shape=input_shape)  # (37, 63)
+    x = layers.Conv1D(64, 3, padding='causal', activation='relu', dilation_rate=1)(inputs)
+    x = layers.Conv1D(64, 3, padding='causal', activation='relu', dilation_rate=2)(x)
+    x = layers.Conv1D(64, 3, padding='causal', activation='relu', dilation_rate=4)(x)
+    x = layers.GlobalAveragePooling1D()(x)     # ← 이게 우리가 뽑을 feature야!
+    x = layers.Dense(128, activation='relu')(x)
+    outputs = layers.Dense(num_classes, activation='softmax')(x)
+
+    model = models.Model(inputs, outputs)
+    return model
+
+
+
+
+# model = models.Sequential([
+#     layers.Input(shape=(37, 63)),
+#     layers.Conv1D(64, kernel_size=3, activation='relu', padding='same'),
+#     layers.MaxPooling1D(pool_size=2),
+#     layers.Conv1D(128, kernel_size=3, activation='relu', padding='same'),
+#     layers.GlobalAveragePooling1D(),
+#     layers.Dense(128, activation='relu'),
+#     layers.Dense(JESTER_CLASSES, activation='softmax')
+# ])
+
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# model.summary()
 
 X_train = X_train.reshape(-1, 37, 21*3)  # 즉, (samples, 37, 63)로 변경
 X_val = X_val.reshape(-1, 37, 21*3)  # 즉, (samples, 37, 63)로 변경
 
+model = build_tcn_model((37, 21*3), JESTER_CLASSES)
 model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_val, y_val))
 
 
