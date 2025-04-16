157685aae9ec614b295cd77e2f8ce7070b7848e9
jungin <jungin@users.noreply.github.com>
2025-04-15 19:09:41 -0500

    split val

--- split_data.py
+++ split_data.py
@@ -1,36 +1,36 @@
 import os
 import shutil
 import pandas as pd
-from variables import data_dir, train_csv, preprocessed_train_dir
+from variables import data_dir, train_csv, preprocessed_train_dir, val_csv
 
 # 15: Swiping Down
 # 9: Shaking Hand
 # 12: Sliding Two Finger Right
-
-df = pd.read_csv(train_csv)
+target = val_csv
+df = pd.read_csv(target)
 new = [9, 12, 15]
 
 # extract three gestures
 new_df = df[df['label_id'].isin(new)]
 
-files = new_df['video_id'].to_list()
-for file in files:
-    filename = file + '.npy'
-    src = os.path.join(preprocessed_train_dir, filename)
-    dst = os.path.join(data_dir, 'new', 'test', filename)
-    if os.path.isfile(src):
-        os.makedirs(os.path.join(data_dir, 'new', 'test'), exist_ok=True)
-        shutil.copy(src, dst)
+# files = new_df['video_id'].to_list()
+# for file in files:
+#     filename = str(file) + '.npy'
+#     src = os.path.join(preprocessed_train_dir, filename)
+#     dst = os.path.join(data_dir, 'new', 'test', filename)
+#     if os.path.isfile(src):
+#         os.makedirs(os.path.join(data_dir, 'new', 'test'), exist_ok=True)
+#         shutil.copy(src, dst)
 
 # not in the new gestures
 not_new = df[~df['label_id'].isin(new)]
 files = not_new['video_id'].to_list()
 for file in files:
-    filename = file + '.npy'
+    filename = str(file) + '.npy'
     src = os.path.join(preprocessed_train_dir, filename)
-    dst = os.path.join(data_dir, 'new', 'train', filename)
+    dst = os.path.join(data_dir, 'new', 'val', filename)
     if os.path.isfile(src):
-        os.makedirs(os.path.join(data_dir, 'new', 'train'), exist_ok=True)
+        os.makedirs(os.path.join(data_dir, 'new', 'val'), exist_ok=True)
         shutil.copy(src, dst)
 
 
