b671c532b5cc3a3fe7c9468c14763af46766d7ce
jungin <jungin@users.noreply.github.com>
2025-04-15 19:00:02 -0500

    extract three gesture for few shot learning

--- split_data.py
+++ split_data.py
@@ -1,5 +1,7 @@
+import os
+import shutil
 import pandas as pd
-from variables import train_csv
+from variables import data_dir, train_csv, preprocessed_train_dir
 
 # 15: Swiping Down
 # 9: Shaking Hand
@@ -11,6 +13,24 @@ new = [9, 12, 15]
 # extract three gestures
 new_df = df[df['label_id'].isin(new)]
 
+files = new_df['video_id'].to_list()
+for file in files:
+    filename = file + '.npy'
+    src = os.path.join(preprocessed_train_dir, filename)
+    dst = os.path.join(data_dir, 'new', 'test', filename)
+    if os.path.isfile(src):
+        os.makedirs(os.path.join(data_dir, 'new', 'test'), exist_ok=True)
+        shutil.copy(src, dst)
 
+# not in the new gestures
+not_new = df[~df['label_id'].isin(new)]
+files = not_new['video_id'].to_list()
+for file in files:
+    filename = file + '.npy'
+    src = os.path.join(preprocessed_train_dir, filename)
+    dst = os.path.join(data_dir, 'new', 'train', filename)
+    if os.path.isfile(src):
+        os.makedirs(os.path.join(data_dir, 'new', 'train'), exist_ok=True)
+        shutil.copy(src, dst)
 
 
